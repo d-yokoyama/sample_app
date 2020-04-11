81036b01113de4dec5abfbd28124b986651f7866
d-yokoyama <d88.yokoyama@gmail.com>
2020-04-12 00:02:08 +0900

    Add coment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  #saveする前にemail_addressを小文字にする
   before_save { self.email = email.downcase}
   validates :name, presence: true, length: { maximum: 50 }
+  #正規表現を定数に定義する
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
